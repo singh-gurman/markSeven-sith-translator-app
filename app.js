78309402a84b6a175cfe43f267256f534cb0bbab
singh-gurman <singh.gurmanp@gmail.com>
2023-07-09 13:39:42 +0530

    update: app.js with mock server
    Added eventListener to respond to the click of the translate button.
    Created functions for appending text to the mock server URL , for parsing the fetched response and displaying it in the  output field on the webpage.

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-var inputBox = document.querySelector("#input-box");
-var translateBtn = document.querySelector("#btn-translate");
-var outputBox = document.querySelector("#output-box");
+const inputBox = document.querySelector("#input-box");
+const translateBtn = document.querySelector("#btn-translate");
+const outputBox = document.querySelector("#output-box");
+
+const serverUrl =
+  "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function completeUrl(text) {
+  return serverUrl + "?" + "text=" + text;
+}
+
+function jsonParse(response) {
+  return (response = response.json());
+}
+
+function showOutput(json) {
+  return (outputBox.innerText = json.contents.translated);
+}
+
+function clickHandler() {
+  const inputText = inputBox.value;
+  fetch(completeUrl(inputText)).then(jsonParse).then(showOutput);
+}
+
+translateBtn.addEventListener("click", clickHandler);
