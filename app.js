62e95665f9b6693a195cbcf43bd063495a3e756a
singh-gurman <singh.gurmanp@gmail.com>
2023-07-09 14:00:54 +0530

    test : created and tested errorHandler
    Created a errorHandler function and tested it by using an incorrect URL as the serverUrl in the code.

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const inputBox = document.querySelector("#input-box");
 const translateBtn = document.querySelector("#btn-translate");
 const outputBox = document.querySelector("#output-box");
 
-const serverUrl =
-  "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+const serverUrl = "https://lessonfourapi.tanaypratap.repl.coa.json";
 
 function completeUrl(text) {
   return serverUrl + "?" + "text=" + text;
@@ -17,9 +16,17 @@ function showOutput(json) {
   return (outputBox.innerText = json.contents.translated);
 }
 
+function errorHandler(error) {
+  console.log("Error occured: " + error);
+  alert("The Server is not responding, please try again later!");
+}
+
 function clickHandler() {
   const inputText = inputBox.value;
-  fetch(completeUrl(inputText)).then(jsonParse).then(showOutput);
+  fetch(completeUrl(inputText))
+    .then(jsonParse)
+    .then(showOutput)
+    .catch(errorHandler);
 }
 
 translateBtn.addEventListener("click", clickHandler);
